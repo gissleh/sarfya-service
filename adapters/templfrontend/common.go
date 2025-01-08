0adef46050dffbf599a6746c1bc9fd21b61e2f10
Gisle Aune <dev@gisle.me>
2025-01-08 18:50:27 +0100

    upgrade templ dependency, add/fix examples and touch up front page a little.

--- adapters/templfrontend/common.go
+++ adapters/templfrontend/common.go
@@ -24,12 +24,12 @@ func findLanguage(match sarfya.FilterMatch, langs string) string {
 func createDemo(dictionary sarfya.Dictionary) *sarfya.FilterMatch {
 	demoData, err := sarfya.NewExample(context.Background(), sarfya.Input{
 		ID:   "demo",
-		Text: "1Kaltxì, 2ulte 3zola'u 4nìprrte' 5fìweptseng6ne! 7(Pamrel si) 8lì'uor 9nefä 10fu 11takuk 12pumit 17a 13mì 14fìpamrel 15fte 16fwivew. 17Nga 18tsun 19kop 20mivay' 21sìkenongit 22a 23fìpamreläo.",
+		Text: "1Kaltxì, 2ulte 3zola'u 4nìprrte' 5fìweptseng6ne! 7(Pamrel si) 8lì'uor 9nefä 10fu 11takuk 12pumit 25a 13mì 14fìpamrel 15fte 16fwivew 24sìkenongit 26lefkeytongay. 17Fko 18tsun 19kop 20mivay' 21sìkenongit 22a 23fìpamreläo.",
 		LookupFilter: map[int]string{
 			11: "vtr.",
 		},
 		Translations: map[string]string{
-			"en": "1Hello, 2and 3+4(welcome) 6to 5(this website)! 7Write 8(a word) 9above 10or 11click 12one 17+13in 14(this text) 15to 16search. 17You 18can 19also 20(check out) 21(the examples) 22+23(below this text.)"},
+			"en": "1Hello, 2and 3+4(welcome) 6to 5(this website)! 7Write 8(a word) 9above 10or 11click 12one 25+13in 14(this text) 15to 16(search for) 26canon 24examples. 17You 18can 19also 20(check out) 21(the examples) 22+23(below this text.)"},
 		Source: sarfya.Source{},
 	}, dictionary)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -1,15 +1,17 @@
 module github.com/gissleh/sarfya-service
 
-go 1.22.2
+go 1.23
+
+toolchain go1.23.4
 
 require (
-	github.com/a-h/templ v0.2.793
+	github.com/a-h/templ v0.3.819
 	github.com/aws/aws-lambda-go v1.47.0
 	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
 	github.com/fwew/fwew-lib/v5 v5.19.6
 	github.com/gissleh/sarfya v1.0.0
 	github.com/labstack/echo/v4 v4.12.0
-	golang.org/x/sync v0.8.0
+	golang.org/x/sync v0.10.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -24,9 +26,9 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 )
